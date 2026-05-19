d09a3b26349fb4e2dfbaf577600836e5e293850d
dnhphuong-05 <vnbt0703@gamil.com>
2026-05-19 21:46:59 +0700

    Upload toàn bộ project lên GitHub

---
+++ app/src/main/java/com/example/baitap/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.baitap;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.baitap.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        connectEvent();
+
+    }
+
+    private void connectEvent() {
+
+    }
+}
